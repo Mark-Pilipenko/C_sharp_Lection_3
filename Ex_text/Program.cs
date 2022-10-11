7658182c1aebd53d6639b2b3dd24cb38b3bd27a7
Mark <kalmark@mail.ru>
2022-10-11 22:43:52 +0300

    Add Ex_text

---
+++ Ex_text/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан текст.
+// Заменить все пробелы черточками,
+// маленькие "к" большими "К",
+// а большие "С" - маленькими "с".
+
+string text = "Сегодня мы углубимся в тему и выделим 4 основных группы методов, "
+                + "а также будем использовать их на практике. И познакомимся с "
+                + "новыми языковыми конструкциями, в частности, циклом.";
+
+string Replace(string text, char OldValue, char NewValue)
+{
+    string result = String.Empty;
+    int count = text.Length;
+    for (int i = 0; i < count; i++)
+    {
+        if (text[i] == OldValue) result = result + $"{NewValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string NewText = Replace(text, ' ', '|');
+NewText = Replace(NewText, 'к', 'К');
+NewText = Replace(NewText, 'С', 'с');
+Console.WriteLine(NewText);
